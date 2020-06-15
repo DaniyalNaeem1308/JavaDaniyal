313e011041154f186fb6415de04b6e7d9cf94274
DaniyalNaeem1308 <daniyal000.ds@gmail.com>
2020-06-15 23:25:59 +0500

    Java is Ready to show

---
+++ app.js
@@ -0,0 +1,165 @@
+// TASK 1 
+
+// var x = ("Welcome to D Packaging")
+// alert(x);
+
+
+
+// alert("Error! Please enter a valid password");
+
+
+
+
+// alert("Welcome to JS Land...\nHappy Codings!");
+
+
+
+
+// alert("Welcome to JS Land...");
+
+
+
+
+// alert("Happy Codings\n Prevent this page for creating additional dialogs. !");
+
+
+
+// Task 2 
+
+// var a = "Username"
+
+// var a = "My Name";
+// var b = "Daniyal Naeem";
+// alert(a +" is "+ b);
+
+
+// var message = "Hello World ";
+
+// var customMess = message;
+// alert(customMess);
+
+
+
+// var Name = "Jhone Doe"
+// var age = " 15 years old "
+// var Position = " Certified Mobile Application Development "
+// alert (Name);
+// alert (age);
+// alert (Position);
+
+// var v = "PIZZA\n ";
+// var w = "PIZZ\n ";
+// var x = "PIZ\n ";
+// var y = "PI\n ";
+// var z= "P\n ";
+
+
+// var customMess = v+w+x+y+z;
+// alert(customMess);
+
+// var a = "My email address";
+// var b = "example@example.com";
+// alert(a +" is "+ b);
+
+// var book = " I am trying to learn from the Book A Smarter Way to learn JavaScript"
+// alert(book);
+
+
+// document.write("Yah! I can write HTML content through JavaScript")
+
+
+// Task 3 
+
+
+// var a = "I am ";
+// var age = "24 ";
+// var c = "years old ";
+// var customMess = a+age+c;
+
+// alert(customMess);
+
+
+// var age = 24
+// var res = "I am ".concat(age , "  ", "years old")
+// alert(res)
+
+// var n = 14
+// var m = " You have visited this site".concat(" ", n, " " , "times")
+// alert(m)
+
+// var birthyear = 1995
+// var mes = "My Birth year is".concat(birthyear+"<br />"+ "Data type of my declared variable is number")
+// document.write(mes)
+
+
+
+
+// var mes = "<b>Jhone</b> ordered <b>5 T Shirts (s)</b> on XYZ Clothing Store"
+
+// document.write(mes)
+
+
+// Task 4 
+
+
+// var variable1 = "Hello World!";
+// var variable2 = "Testing...";
+// var variable3 = 42;
+
+
+
+// var rulee = "<f><b>Rules for naming JS variables</b></f>"
+// document.write(rulee)
+
+// var a = "numbers"
+// var b = "$"
+// var c = "_"
+// var contt = "Variable names can only contain" .console (a,b and c) For example $my_1stVariable
+// document.write(contt)
+
+
+// Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// Variable names are case _________
+// Variable names should not be JS _________
+
+
+
+// Task 5 
+
+
+
+// var a = 3
+// var b = 5 
+// var c = a + b
+// document.write(c)
+
+// var a = 3
+// var b = 5 
+// var c = a - b
+// document.write(c)
+
+// var a = 3
+// var b = 5 
+// var c = a * b
+// document.write(c)
+
+// var a = 3
+// var b = 5 
+// var c = a / b
+// document.write(c)
+
+
+// var num = 5;
+// var newNum = num++;
+// var neew = 7;
+// var newNum = num++ + neew ;
+// var newNum = num-- + neew ;
+
+// document.write(newNum)
+
+var a = 600;
+var b = 5;
+var c = a*b
+document.write(c)
+
